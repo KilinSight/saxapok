26fa577290ed2bc2174f873584a849602dd91c53
kilinsight <kilinsight@mail.ru>
2020-02-16 22:37:00 +0700

    Entities User Update created

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -102,7 +102,7 @@ class DefaultController extends Controller
         $telegramManager = $this->get(TelegramManager::class);
         $updateRaw = $telegramManager->getUpdateRaw();
         $update = $telegramManager->getUpdateMetadata($updateRaw);
-        $telegramManager->notifyAdmins(json_encode($updateRaw));
+//        $telegramManager->notifyAdmins(json_encode($updateRaw));
         if($update->getDate()->getTimestamp() > (time() - 5)){
             if(!$update->getUser()->getIsBot() && !isset($updateRaw['message']['forward_from'])){
                 $telegramManager->forwardToAdmin($update->getUser()->getUserId(), $update->getMessageId());
