868fd6f59d199b4a3255b67e4b0b6e991a9a4682
kilinsight <kilinsight@mail.ru>
2020-02-19 21:05:16 +0700

    Reply WIP

--- src/AppBundle/Manager/TelegramManager.php
+++ src/AppBundle/Manager/TelegramManager.php
@@ -159,14 +159,14 @@ class TelegramManager
      */
     public function getOrCreateUser(int $userId, string $username, ?string $firstname = null, ?string $lastname = null, ?bool $isBot = false)
     {
-        $issetUser = $this->em->getRepository(TelegramUser::class)->findOneBy(['userId' => $userId]);
-        if(!$issetUser){
+        $issetUser = $this->em->getRepository(TelegramUser::class)->findBy(['userId' => $userId]);
+        if(!$issetUser || count($issetUser) === 0){
             $tgUser = new TelegramUser(null, intval($userId), $username, $firstname, $lastname, $isBot);
             $this->em->persist($tgUser);
             $this->em->flush();
             return [false, $tgUser];
         }else{
-            return [true, $issetUser];
+            return [true, $issetUser[0]];
         }
     }
 
